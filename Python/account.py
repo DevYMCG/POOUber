aa68d2fe979b845be4613368489ad7d7c4bbe385
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-31 17:23:35 -0400

    create class account, car, main and route

---
+++ Python/account.py
@@ -0,0 +1,10 @@
+from ast import Str
+from xml.dom.minidom import Document
+
+
+class Account:
+    id          = int
+    name        = Str
+    Document    = Str
+    email       = Str
+    password    = Str
---
+++ Python/car.py
@@ -0,0 +1,8 @@
+from ast import Str
+
+
+class Card:
+    id              = int
+    license         = Str
+    driver          = Str
+    passengenger    = int
