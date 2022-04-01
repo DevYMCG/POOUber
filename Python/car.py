d9e6fbd76af98d238320b5873684af04fec498ec
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-31 22:26:36 -0400

    adding constructor

--- Python/car.py
+++ Python/car.py
@@ -1,8 +1,13 @@
 from ast import Str
+from account import Account
 
 
 class Car:
     id              = int
     license         = Str
-    driver          = Str
+    driver          = Account("", "")
     passengenger    = int
+
+    def __init__(self, license, driver):
+        self.license = license
+        self.driver = driver
