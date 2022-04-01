3fd5f1e5c78f032e8cd019fe04e403b2af5fc80f
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-01 00:24:07 -0400

    adding constructor

--- PHP/Car.php
+++ PHP/Car.php
@@ -1,8 +1,16 @@
 <?php
 class Car{
+
     public $id = integer;
     public $license = string;
     public $document = string;
     public $driver = string;
     public $passengers = string;
+
+    public function __construct(license, driver){
+
+        $this->license = $license;
+        $this->driver = $driver;
+
+    }
 }
