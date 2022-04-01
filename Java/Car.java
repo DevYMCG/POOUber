04119626526c47ec523dc2b8edf4030edc29b151
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-31 21:21:43 -0400

    adding constructor

--- Java/Car.java
+++ Java/Car.java
@@ -4,8 +4,20 @@ class Car{
 
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passegenger;
+
+    /*Constructor:
+    
+    Parametros obligatorios para que se cree mi objeto
+    de tipo Car
+    
+    */
+
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+    }
 
     void printDataCar(){
         System.out.println("License: "+ license + " Driver: "+ driver);
