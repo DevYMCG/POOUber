fe89cb0d1f2af78f09dff2ee6c75b267fab32d55
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-01 11:02:56 -0400

    add inheritance

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,9 @@ car.printDataCar();
 var uberx = new UberX("AW456", new Account("Yesenia Araque", "DOCT123"), "chevrolet", "Spark");
 uberx.passenger = 4;
 uberx.printDataCar();
+
+var user = new User("Jose Tapia", "VENEZ456");
+user.printDataAccount();
+
+var driver = new Driver("ceta martinez", "DESAFIOTHEBOX");
+driver.printDataAccount();
