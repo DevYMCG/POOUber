461226841a9687d649cedbc1f67a59bc436d8715
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-01 09:54:09 -0400

    egregating inheritance

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,18 @@
 from car import Car
 from account import Account
+from UberX import UberX
 
 
 def run():
    car = Car("GTHY369", Account("Andres Herrera", "ANDA876"))
    print(vars(car))
    print(vars(car.driver))
+
+   uberx = UberX("GTHY369", Account("Jose Carrero", "UYKM876"))
+   print(vars(uberx))
+   print(vars(uberx.driver))
+
+
 
 
 if __name__ == "__main__":
