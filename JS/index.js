e510a74ce29ff99f38328a5c12ab8e9dacdb5bc3
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-01 10:11:25 -0400

    egregating inheritance

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andres Herrera", "DOCT123"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberx = new UberX("AW456", new Account("Yesenia Araque", "DOCT123"), "chevrolet", "Spark");
+uberx.passenger = 4;
+uberx.printDataCar();
