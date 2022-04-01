e90fa72ce19fa72cb2b61acd8b5029058e7aae2a
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-31 22:24:06 -0400

    adding constructor

--- Python/account.py
+++ Python/account.py
@@ -5,6 +5,10 @@ from xml.dom.minidom import Document
 class Account:
     id          = int
     name        = Str
-    Document    = Str
+    document    = Str
     email       = Str
     password    = Str
+
+    def __init__(self, name, document):
+        self.name = name
+        self.document = document
