88dabf512dca0eb3d62313a31025b1cd9dcbf5e2
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-01 08:41:57 -0400

    egregating inheritance

---
+++ Java/UberVan.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+class UberVan extends Car{
+    
+    Map<String, Map<String, Integer>>  typeCardAccepted;
+    ArrayList<String> seatsMaterial;
+
+    public UberVan(String license, Account driver, Map<String, Map<String, Integer>>  typeCardAccepted, ArrayList<String> seatsMaterial){
+        super(license, driver);
+        this.typeCardAccepted = typeCardAccepted;
+        this.seatsMaterial = seatsMaterial;
+    }
+}
